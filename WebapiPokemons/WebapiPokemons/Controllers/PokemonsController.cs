3cb22aa7ad1169bdee30983cf9dcae5d66dd50ff
Abraraw Farre Violan <abraraw.farre@ohtic.com>
2024-10-30 15:28:15 +0100

    controllador-pokemon-done,

---
+++ WebapiPokemons/WebapiPokemons/Controllers/PokemonsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebapiPokemons.Entidades;
+
+namespace WebapiPokemons.Controllers
+{
+    [ApiController]
+    [Route("api/pokemons")]
+    public class PokemonsController: ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<List<Pokemon>> Get()
+        {
+            return new List<Pokemon>()
+            {
+                new Pokemon() { Id = 1, Nombre = "Felipe"},
+                new Pokemon() { Id = 2, Nombre = "Joan"}
+            };
+        }
+
+    }
+}
---
+++ WebapiPokemons/WebapiPokemons/Entidades/Pokemon.cs
@@ -0,0 +1,9 @@
+﻿namespace WebapiPokemons.Entidades
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+    }
+}
